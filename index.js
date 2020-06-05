bb0155d2dff60ad94da158ed6cbdf0e8a448ec2a
cedric.chaffard@gmail.com <cedric.chaffard@gmail.com>
2020-06-05 14:12:39 +0200

    index.js corrections

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const btnToggle = document.querySelector('.btn-toggle');
+const btnToggle = document.querySelector(".btn-toggle");
 
-btnToggle.addEventListener('click', () => {
+btnToggle.addEventListener("click", () => {
 
     const body = document.body;
 
-    if(body.classList.contains('dark')){
+    if(body.classList.contains("dark")){
 
-        body.classList.add('light')
-        body.classList.remove('dark')
+        body.classList.add("light")
+        body.classList.remove("dark")
         btnToggle.innerHTML = "Go Dark"
 
-    }else if(body.classList.contains('light')){
+    }else if(body.classList.contains("light")){
 
-        body.classList.add('dark')
-        body.classList.remove('light')
+        body.classList.add("dark")
+        body.classList.remove("light")
         btnToggle.innerHTML = "Go Light"
 
     }
